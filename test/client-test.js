fa263ce3a634ee656e90ceca8f0b504889a95779
samchen08 <samchen08@163.com>
2016-08-05 14:32:04 +0800

    add karma jasmine demo

---
+++ test/client-test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {renderIntoDocument} from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+
+
+describe('test', () => {
+    it('test spec', () => {
+        expect(true).toBeTruthy();
+
+    });
+
+
+});
