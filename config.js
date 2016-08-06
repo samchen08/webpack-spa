3627147dc5f0c2e5aadbded07b548003209a095b
samchen08 <samchen08@163.com>
2016-08-06 21:42:28 +0800

    add plugins

--- config.js
+++ config.js
@@ -69,7 +69,7 @@ let config = {
     pages: [
         {
             title: 'hello world',
-            template: 'templates/base.html',
+            template: 'templates/index.html',
             filename: 'index.html',
             chunks: ['app', 'lib'],
             minify: true
